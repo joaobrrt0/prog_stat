7a4d80b14f1ecb1f5bb6ed7fd50f4177d1bca357
gmdrs <gmmlima@id.uff.br>
2026-08-24 09:45:52 -0300

    inicialize the second activity from the course

---
+++ atividade_2.r
@@ -0,0 +1,6 @@
+dados = read.csv("dados_turismo.csv", header= T, sep = ".")
+str(dados)
+summary(dados)
+dados$sexo = factor(dados$sexo, levels = c(1, 2), labels = c("masculino", "feminino"))
+dados$estabelecimento = factor(dados$estabelecimento, levels = c("P", "H"), labels = c("pousada", "hotel"))
+

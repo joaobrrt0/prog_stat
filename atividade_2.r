173b2b90d2fe683c1e1fba411ef189de2163e628
gmdrs <gmmlima@id.uff.br>
2026-08-24 10:11:31 -0300

    final step

--- atividade_2.r
+++ atividade_2.r
@@ -17,4 +17,9 @@ f_sexo_estab = round(prop.table(table(dados$sexo, dados$estabelecimento, useNA =
 barplot(f_sexo_estab, main = "Distribuição dos turistas segundo sexo por tipo de estabelecimento",
         xlab = "tipo de estabelecimento", ylab = "%", beside = TRUE)
 
-
+quantile(dados$duracao, c(0.25, 0.50, 0.75), na.rm = TRUE)
+mean(dados$duracao, na.rm = TRUE)
+sd(dados$duracao, na.rm = TRUE)
+quantile(dados$valor, c(0.25, 0.50, 0.75), na.rm = TRUE)
+mean(dados$valor, na.rm = TRUE)
+sd(dados$valor, na.rm = TRUE)

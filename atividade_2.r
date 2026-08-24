88b2af4b73967769b88bf5587d500ec8a40c242d
gmdrs <gmmlima@id.uff.br>
2026-08-24 10:02:51 -0300

    add descritive analysis into the code and a plot

--- atividade_2.r
+++ atividade_2.r
@@ -1,6 +1,20 @@
-dados = read.csv("dados_turismo.csv", header= T, sep = ".")
+dados = read.csv("dados_turismo.csv", header = TRUE, sep = ",")
 str(dados)
 summary(dados)
 dados$sexo = factor(dados$sexo, levels = c(1, 2), labels = c("masculino", "feminino"))
 dados$estabelecimento = factor(dados$estabelecimento, levels = c("P", "H"), labels = c("pousada", "hotel"))
+
+f_sexo = round(prop.table(table(dados$sexo, useNA = "no")) * 100, 2)
+barplot(f_sexo, main = "Distribuição de frequências dos turistas segundo sexo",
+        xlab = "sexo", ylab = "%")
+
+f_estab = round(prop.table(table(dados$estabelecimento, useNA = "no")) * 100, 2)
+barplot(f_estab, main = "Distribuição de frequências dos turistas segundo tipo de estabelecimento de estadia",
+        xlab = "tipo de estabelecimento", ylab = "%")
+
+f_sexo_estab = round(prop.table(table(dados$sexo, dados$estabelecimento, useNA = "no"),
+                               margin = 2) * 100, 2)
+barplot(f_sexo_estab, main = "Distribuição dos turistas segundo sexo por tipo de estabelecimento",
+        xlab = "tipo de estabelecimento", ylab = "%", beside = TRUE)
+
 
